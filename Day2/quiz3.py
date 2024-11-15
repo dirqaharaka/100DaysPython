e1d37bede1e1ecf66a0e9bde1d7fc168e734f68d
dirqaharaka <dirkaharaka77@gmail.com>
2024-11-15 17:11:14 +0800

    update project2

--- Day2/quiz3.py
+++ Day2/quiz3.py
@@ -1,25 +1,44 @@
+# Mathematical Operations Quiz
+# Quiz 3|3 questions
+
+# We've covered a lot of mathematical operations you can do with Python. e.g. round(), floor division //, PEMDAS etc. This quiz is going to check to see if you can use all these concepts correctly.
+
 # Question 1:
 
-# Which statement below is incorrect?
-    # 932 is an Integer
-    # "False" is a Boolean  <--(Answer)
-    # 857.24 is a Float
-    # "523" is a String
+# You are a computer. What will this line of code print?
+
+# print(6 + 4 / 2 - (1 * 2)) 
+
+# 3
+# 6.0     <----- Answer
+# 8.0
+# 5
 
 # Question 2:
 
-# What is the data type of the mystery variable?
-# mystery = 734_529.678 
+# What is the data type of the result of the variable a in the following line of code:
 
-    # Integer
-    # Syntax
-    # Qurtle
-    # Float   <--(Answer)
+# a = int("5") / int(2.7) 
+
+# int
+# float   <---- Answer
+# str
+# bool
+
 
 # Question 3:
 
-# I've put a spell on you. You are now a computer. If I give you the following code, what will you print out?
-#     street_name = "Abbey Road"
-#     print(street_name[4] + street_name[7])
+# Which of these lines of code will give you an error?
 
-# Answer = yo
+# name = input("What is your name?")
+# print(f"Hello, (name)")
+
+# name = input("What is your name?") 
+# print("Hello," + name)
+
+# age = 12
+# print(f"You are {age} years old")
+
+# age = 12
+# print("You are " + age + "years old")           <======= Answer
+
